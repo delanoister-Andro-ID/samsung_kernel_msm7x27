16ef5c531f4bd474f9ab8d02d7f4b10d910cb022
Erick <delano.erick@gmail.com>
2012-05-23 20:45:49 +0700

    update kernel msm

---
+++ source/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 33 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue May 15 17:39:26 WIT 2012"
+#define LINUX_COMPILE_TIME "17:39:26"
+#define LINUX_COMPILE_BY "delanoister"
+#define LINUX_COMPILE_HOST "dev-laptop"
+#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
